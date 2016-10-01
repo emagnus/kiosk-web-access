ebf1823d76ff0aec94327a9e8d5a19376b9d992b
Magnus Westergaard <westerga@gmail.com>
2016-10-01 13:46:43 +0200

    Indentation issues.

--- kiosk_web_server.py
+++ kiosk_web_server.py
@@ -5,26 +5,26 @@ import os
 import subprocess
 
 class KioskWebAccessHandler(BaseHTTPRequestHandler):
-	def do_GET(self):
-	  if self.path.endswith('default'):
-	  	try:
-	  		f = open('default.url.cfg')
-	  		default_url = f.read()
+  def do_GET(self):
+    if self.path.endswith('default'):
+      try:
+        f = open('default.url.cfg')
+        default_url = f.read()
 
-		    #send code 200 response
-		    self.send_response(200)
+        #send code 200 response
+        self.send_response(200)
 
-		    #send header first
-		    self.send_header('Content-type','text-html')
-		    self.end_headers()
+        #send header first
+        self.send_header('Content-type','text-html')
+        self.end_headers()
 
-		    subprocess.call(['./go_to_url.sh', default_url])
+        subprocess.call(['./go_to_url.sh', default_url])
 
-		    #send file content to client
-		    self.wfile.write('Default URL (' + default_url + ') successfully loaded.')
-		    return
-		  except IOError:
-      	self.send_error(500, 'Error reading default URL file.')
+        #send file content to client
+        self.wfile.write('Default URL (' + default_url + ') successfully loaded.')
+        return
+      except IOError:
+        self.send_error(500, 'Error reading default URL file.')
 
 def run():
   print('Kiosk Web Access HTTP server is starting...')
